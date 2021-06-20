59ed51dab0a331adbac2981625f7575d5705820c
kazuya0413 <85945700+kazuya0413@users.noreply.github.com>
2021-06-20 10:54:07 +0900

    portfolio
    ポートファリオ

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function() {
+  // wow.js
+    new WOW().init();
+  
+    
+   // #から始まるURLがクリックされた時
+  jQuery('a[href^="#"]').click(function() {
+   // 移動速度を指定（ミリ秒）
+   let speed = 300;
+   // hrefで指定されたidを取得
+   let id = jQuery(this).attr("href");
+   // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
+   let target = jQuery("#" == id ? "html" : id);
+   // ページのトップを基準にターゲットの位置を取得
+   let position = jQuery(target).offset().top;
+   // ターゲットの位置までspeedの速度で移動
+   jQuery("html, body").animate(
+     {
+       scrollTop: position
+    },
+     speed
+   );
+   return false;
+  });
+  
+});
+ 
